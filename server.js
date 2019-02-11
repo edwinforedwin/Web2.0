747c339a45b8b722b27ee7edd693eea60e15eb58
Edwin U Kannanaikkal <edwinforedwin@gmail.com>
2019-02-11 20:44:07 +0530

    Posting With Postman

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/',(req,res)=> {
-    var email = req.body.email;
-    var amount = req.body.amount;
+/* Handling all Parsing */
+app.use(bodyParser.json());
 
-    res.send({"Amount" : amount, "Email" : email});
-})
+app.post('/',(req,res)=> {
+var email = req.body.email;
+var amount = req.body.amount;
+
+res.send({"amount" : amount, "email" : email});
+});
+
 app.listen(3000,()=> {
     console.log('Server is running in Port 3000');
 });
